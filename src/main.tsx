6cb01c5ed84de95e725bd8cb8b5f39690d36c097
Lucas Rezende <lucas.minatelli@gmail.com>
2022-06-26 12:07:09 -0300

    hw request

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { client } from "./lib/apollo";
 import Home from "./pages/Home";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home />
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,18 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POSTS = gql`query MyQuery {
+  posts {
+    publishedAt
+    title
+    coverImage {
+      url
+    }
+  }
+}
+`
+
 const Home = () => {
+  const {data} = useQuery(GET_POSTS);
   return <div>Home</div>;
 };
 
