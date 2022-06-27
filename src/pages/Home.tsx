eb1e9c145e08c1573dfde2eefb262be393600cc5
Lucas Rezende <lucas.minatelli@gmail.com>
2022-06-27 08:55:42 -0300

    codegen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
 import Instagram from "../components/Cards/Instagram";
 
-const GET_POSTS = gql`
-  query GetPosts {
-    posts {
-      publishedAt
-      title
-      coverImage {
-        url
-      }
-      author {
-        name
-        picture {
-          url
-        }
-      }
-    }
-  }
-`;
-
 const Home = () => {
-  const { data } = useQuery(GET_POSTS);
+  const { data } = [];
   if (!data) return <h1>Houve um erro!</h1>;
   return (
     <>
